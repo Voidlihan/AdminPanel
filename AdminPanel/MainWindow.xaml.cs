4a80d5b61d7ce3b94c288f7395b09285ec4d459c
e.alihangt@gmail.com <e.alihangt@gmail.com>
2019-12-11 21:40:22 +0600

    Add project files.

---
+++ AdminPanel/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AdminPanel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SaveClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EditClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
